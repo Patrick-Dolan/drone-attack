a62026906aa8f35b1e7df34cebe6d768a9cbcb9e
Patrick Dolan <dolanp1992@gmail.com>
2022-07-20 21:04:04 -0700

    added skybox and a small amount of fog

--- Assets/Scripts/GroundMovement.cs
+++ Assets/Scripts/GroundMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GroundMovement : MonoBehaviour
 {
-    private Vector3 loopPoint = new Vector3(0, 0, -24);
+    private Vector3 loopStartPoint = new Vector3(0, 0, -24);
+    private int loopEndPoint = -72;
     public float speed = 3.0f;
 
     // Start is called before the first frame update
@@ -16,9 +17,9 @@ public class GroundMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z <= -72)
+        if (transform.position.z <= loopEndPoint)
         {
-            transform.position = loopPoint;
+            transform.position = loopStartPoint;
         }
         transform.Translate(Vector3.back * Time.deltaTime * speed);
     }
