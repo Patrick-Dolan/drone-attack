a220a8c6a0c6b1013ba6f96685c9d8b685a24fb3
Patrick Dolan <dolanp1992@gmail.com>
2022-07-28 18:57:17 -0700

    change target behavior to move in close and slow down

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private float xRange = 5.0f;
-    private float ySpawnPosMax = 5.0f;
-    private float ySpawnPosMin = 0.0f;
+    private float xRange = 8.0f;
+    private float ySpawnPosMax = 8.0f;
+    private float ySpawnPosMin = 1.0f;
     private float zSpawnPos = 50.0f;
-    private float speed = 10.0f;
-    public int scoreValue;
+    private float speed = 15.0f;
+    private int droneSightDistance = 20;
     private GameManager gameManager;
     private Transform camera;
 
@@ -25,8 +25,9 @@ public class Target : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
-        if (transform.position.z < 15)
+        if (transform.position.z < droneSightDistance)
         {
+            speed = 10.0f;
             Quaternion cameraRotation = Quaternion.LookRotation(camera.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, speed * Time.deltaTime);
             //transform.LookAt(camera);
